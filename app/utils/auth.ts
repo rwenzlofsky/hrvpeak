4363c56500b51c0f8b28b6e764bccc8ad241fb69
Roland Wenzlofsky <r.wenzlofsky@gmail.com>
2024-03-16 00:13:25 +0000

    WIP

--- app/utils/auth.ts
+++ app/utils/auth.ts
@@ -1,6 +1,9 @@
-import { NextAuthOptions } from "next-auth";
+import type { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
 
 export const authOptions = {
+
     debug: true,
     providers: [
         {
@@ -34,7 +37,12 @@ export const authOptions = {
 
                 };
             },
-        }
+        },
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+        })
+
     ],
     callbacks: {
 
