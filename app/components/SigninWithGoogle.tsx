b257cf9eed1dfd9e939d76eb7b6c8be451a3c24a
Roland Wenzlofsky <r.wenzlofsky@gmail.com>
2024-03-19 22:49:42 +0000

    wip

--- app/components/SigninWithGoogle.tsx
+++ app/components/SigninWithGoogle.tsx
@@ -1,16 +1,20 @@
-'use client'
+"use client";
 
 import { Button } from "@/components/ui/button";
 import { signIn } from "next-auth/react";
 
 export default function SigninWithGoogle() {
-
-    return (
-        <Button onClick={() => signIn('google', {
-            callbackUrl: "https://hrvpeak.com"
-        }
-        )} className="mt-4" variant="secondary">Login with Google</Button>
-    )
-
-
+  return (
+    <Button
+      onClick={() =>
+        signIn("google", {
+          callbackUrl: "https://hrvpeak.com",
+        })
+      }
+      className="mt-4"
+      variant="secondary"
+    >
+      Login with Google
+    </Button>
+  );
 }
